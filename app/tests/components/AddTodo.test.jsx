244479d67fdb3306d04071aac1d0413c044bb023
Clint Bush <clint@madebyclint.com>
2017-02-21 22:48:33 -0800

    Add basic exist test to AddTodo

---
+++ app/tests/components/AddTodo.test.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import expect from 'expect'
+import $ from 'jQuery'
+import TestUtils from 'react-addons-test-utils'
+
+import AddTodo from 'AddTodo'
+
+describe('AddTodo', () => {
+    it('should exist', () => {
+        expect(AddTodo).toExist()
+    })
+
+})
