a05fa1b526a40e866a8e17b394b41db718e34ce4
Clint Bush <clint.bush@homesite.com>
2017-02-23 11:22:38 -0800

    Fix e.preventDefault on AddTodo

--- app/components/AddTodo.jsx
+++ app/components/AddTodo.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 export default class AddTodo extends React.Component {
     handleSubmit = (e) => {
-        e.preventDefault
+        e.preventDefault()
         let todoText = this.refs.todoText.value
         if (todoText.length > 0) {
             this.refs.todoText.value = ''
