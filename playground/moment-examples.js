faea1e21d732029912b571495e3b5da4ca256f94
Clint Bush <clint@madebyclint.com>
2017-02-23 22:01:51 -0800

    Play around with moment.

--- playground/moment-examples.js
+++ playground/moment-examples.js
@@ -1,3 +1,14 @@
 let moment = require('moment')
 
 console.log(moment().format())
+
+
+//unix timestamp
+let now = moment()
+console.log(now.unix())
+
+let timestamp = now.unix()
+let currentMoment = moment.unix(timestamp)
+console.log(currentMoment.format())
+
+console.log(currentMoment.format('MMM'))
