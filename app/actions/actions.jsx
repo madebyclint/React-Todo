935b8046f8a776dc911f9f9e62005f609483e6a6
Clint Bush <clint@madebyclint.com>
2017-03-11 08:17:07 -0800

    Add actions and actions test

--- app/actions/actions.jsx
+++ app/actions/actions.jsx
@@ -4,3 +4,23 @@ export const setSearchText = (searchText) => {
         searchText
     }
 }
+
+export const addTodo = (text) => {
+    return {
+        type: 'ADD_TODO',
+        text
+    }
+}
+
+export const toggleShowCompleted = () => {
+    return {
+        type: 'TOGGLE_SHOW_COMPLETED'
+    }
+}
+
+export const toggleTodo = (id) => {
+    return {
+        type: 'TOGGLE_TODO',
+        id
+    }
+}
--- app/tests/actions/actions.test.jsx
+++ app/tests/actions/actions.test.jsx
@@ -1,6 +1,5 @@
 import expect from 'expect'
 import * as actions from 'actions'
-// let actions = require('actions')
 
 describe('Actions', () => {
     it('should generate search text action', () => {
@@ -9,6 +8,29 @@ describe('Actions', () => {
             searchText: 'some search text'
         }
         let res = actions.setSearchText(action.searchText)
+        expect(res).toEqual(action)
+    })
+    it('should generate add todo action', () => {
+        var action = {
+            type: 'ADD_TODO',
+            text: 'Thing to do'
+        }
+        let res = actions.addTodo(action.text)
+        expect(res).toEqual(action)
+    })
+    it('should generate toggle show completed action', () => {
+        var action = {
+            type: 'TOGGLE_SHOW_COMPLETED'
+        }
+        let res = actions.toggleShowCompleted()
+        expect(res).toEqual(action)
+    })
+    it('should generate toggle todo action', () => {
+        var action = {
+            type: 'TOGGLE_TODO',
+            id: 2
+        }
+        let res = actions.toggleTodo(action.id)
         expect(res).toEqual(action)
     })
 })
