322bf4887d15e5db1a55c25d0a51eb3b89610688
Clint Bush <clint@madebyclint.com>
2017-03-12 12:55:19 -0700

    Add showCompletedReducer

--- app/tests/reducers/reducers.test.jsx
+++ app/tests/reducers/reducers.test.jsx
@@ -12,4 +12,20 @@ describe('Reducers', () => {
             expect(res).toEqual(action.searchText)
         })
     })
+    describe('showCompletedReducer', () => {
+        it('should toggle showCompleted status from true to false', () => {
+            let action = {
+                type: 'TOGGLE_SHOW_COMPLETED'
+            }
+            let res = reducers.showCompletedReducer(true, action)
+            expect(res).toEqual(false)
+        })
+        it('should toggle showCompleted status from false to true', () => {
+            let action = {
+                type: 'TOGGLE_SHOW_COMPLETED'
+            }
+            let res = reducers.showCompletedReducer(false, action)
+            expect(res).toEqual(true)
+        })
+    })
 })
